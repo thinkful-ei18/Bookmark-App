d8f779b4f45a62fd7258f2c593cc9efe5204233b
admin <Mistermo716@gmail.com>
2018-02-04 17:02:23 -0800

    Expand and Condense implement

--- scripts/book-list.js
+++ scripts/book-list.js
@@ -5,80 +5,81 @@ const bookList = (function(){
   function generateItemElementCon(item) {
 
     let version = 1;
-    let starItemVar = starItem(item,version);
+    let starItemVar = starItem(item.rating);
 
-    return `<li class = "li-mark" data-item-id="${item.id}"><div class ="container1">
+    return `<li class = "li-mark" data-item-star="${item.rating}" data-item-url="${item.url}" data-item-desc="${item.desc}" data-item-title="${item.title}" data-item-id="${item.id}"><div class ="container1">
       <div class ="container2">
-      <span class ="title">${item.title}</span>
+      <span class ="title-mark">${item.title}</span>
       ${starItemVar}
       <span class = "delete-button"><button class ="dltBtn" type="button">Delete Mark</button></span>
-      <span class = "expand-button"><button class ="expBtn" type="button">Expand</button></span>
+      <button class ="expBtn" type="button">Expand</button> </span>
       </div>
   </div></li> `;
 
   }
-  function generateItemElementExp(item){
+  function generateItemElementExp(title, id, desc, url, star){
     let version = 2;
-    let starItemVar = starItem(item, version);
+    let starItemVar = starItem(star);
     return `<div class ="expand-container1">
       <div class ="expand-container2">
-      <span class ="title-expand">Book Title</span>
-      <a class ="link-expand" href="">Link To Page</a>
-      ${starItemVar}
-      <span class = "add-bookmark-expand"><button type="button">Add Bookmark</button></span>
+      <span class ="title-expand">${title}</span>
+      <a class ="link-expand" href="">${url}</a>
+      <span class ="expStar">${starItemVar}</span>
+      <span class = "add-bookmark-expand"><button type="button" class="conBtn">Condense Mark</button></span>
       <span class="description-heading">Description</span>
-      <span class="description-expand">description</span>
+      <span class = "delete-button-exp"><button class ="dltBtn" type="button">Delete Mark</button></span>
+      <span class="description-expand" >${desc}</span>
       </div>
   </div>`;
 
   }
-  function starItem(item, version){
+  function starItem(item){
     let starItem = '';
-    if(version === 1)
+    // if(version === 1)
+    //   {
+      if (item === 1) {
+        starItem  = '<span value="1" class="fa fa-star star star1 checked"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span>';
+      }
+      else if (item === 2){
+        starItem  = '<span value="2" class="fa fa-star star star1 checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span>';
+      }
+      else if (item === 3)
       {
-      if (item.rating === 1) {
-        starItem  = '<span class="fa fa-star star checked"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span>';
+        starItem  = '<span value="3" class="fa fa-star star star1 checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span>';
       }
-      else if (item.rating === 2){
-        starItem  = '<span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span>';
+      else if (item === 4){
+        starItem  = '<span value="4" class="fa fa-star star star1 checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star"></span>';
       }
-      else if (item.rating === 3)
-      {
-        starItem  = '<span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span>';
-      }
-      else if (item.rating === 4){
-        starItem  = '<span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star"></span>';
-      }
-      else if (item.rating === 5) {
-        starItem  = '<span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star checked"></span>';
+      else if (item === 5) {
+        starItem  = '<span value="5" class="fa fa-star star star1 checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star checked"></span>';
       }
       else{
-        starItem = '<span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span>';
+        starItem = '<span value="0" class="fa fa-star star star1"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span>';
       }
-    }
-    else if (version === 2)
-      {
-      if (item.rating === 1) {
-        starItem  = '<span class="fa fa-star star-expand checked"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span>';
-      }
-      else if (item.rating === 2){
-        starItem  = '<span class="fa fa-star star-expand checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span>';
-      }
-      else if (item.rating === 3)
-      {
-        starItem  = '<span class="fa fa-star star-expand checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span>';
-      }
-      else if (item.rating === 4){
-        starItem  = '<span class="fa fa-star star-expand checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star"></span>';
-      }
-      else if(item.rating === 5) {
-        starItem  = '<span class="fa fa-star star-expand checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star checked"></span>';
-      }
-      else {
-        starItem = '<span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span>';
-      }
+    // }
+    // else if (version === 2)
+    //   {
+    //   if (item === 1) {
+    //     starItem  = '<span value="1" class="fa fa-star star-expand star1 checked"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span>';
+    //   }
+    //   else if (item === 2){
+    //     starItem  = '<span value="2" class="fa fa-star star-expand star1 checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span>';
+    //   }
+    //   else if (item === 3)
+    //   {
+    //     starItem  = '<span value="3" class="fa fa-star star-expand star1 checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span>';
+    //   }
+    //   else if (item === 4){
+    //     starItem  = '<span value="4" class="fa fa-star star-expand star1 checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star"></span>';
+    //   }
+    //   else if(item === 5) {
+    //     starItem  = '<span value="5" class="fa fa-star star-expand star1 checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star checked"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star checked"></span>';
+    //   }
+    //   else {
+    //     starItem = '<span value="0" class="fa fa-star star star1"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span> <span class="fa fa-star star"></span>';
+    //   }
 
-    }
+    // }
     return starItem;
   }
    
@@ -109,7 +110,6 @@ const bookList = (function(){
       const newRating = $('.star-sub').val();
       const newDescription = $('.desc-sub').val();
       const curItem = item.create(newItemTitle,newItemURL,newDescription,newRating);
-      console.log(curItem);
       $('description-sub').val('Description');
       $('.link-sub').val('Link To Page');
       $('.title-sub').val('Title');
@@ -125,12 +125,48 @@ const bookList = (function(){
       $('ul').on('click','li .expBtn' ,function(event){
         event.preventDefault();
         console.log('expand clicked');
+        const title = $(event.currentTarget).closest('.li-mark').data('item-title');
         const id = $(event.currentTarget).closest('.li-mark').data('item-id');
-
+        const desc = $(event.currentTarget).closest('.li-mark').data('item-desc');
+        const url = $(event.currentTarget).closest('.li-mark').data('item-url');
+        const star = $(event.currentTarget).closest('.li-mark').data('item-star');
         api.getItems((item)=>{
           const result = item.filter(item => item.id === id); 
-          const expandedVersion= generateItemElementExp(result);
+          const expandedVersion = generateItemElementExp(title, id, desc, url, star);
+          console.log(expandedVersion);
           $(event.currentTarget).closest('.li-mark').html(expandedVersion);
+
+
+
+        });
+
+        
+
+      });
+
+    }
+
+    function handleCondensedItem(){
+      $('ul').on('click','li .conBtn' ,function(event){
+        event.preventDefault();
+        console.log('expand clicked');
+        const title = $(event.currentTarget).closest('.li-mark').data('item-title');
+        const id = $(event.currentTarget).closest('.li-mark').data('item-id');
+        const desc = $(event.currentTarget).closest('.li-mark').data('item-desc');
+        const url = $(event.currentTarget).closest('.li-mark').data('item-url');
+        const star = $(event.currentTarget).closest('.li-mark').data('item-star');
+        const objCon = {
+          title,
+          id,
+          desc,
+          url,
+          rating: star
+        };
+        api.getItems((item)=>{
+          const result = item.filter(item => item.id === id); 
+          const conVersion = generateItemElementCon(objCon);
+          console.log(conVersion);
+          $(event.currentTarget).closest('.li-mark').html(conVersion);
 
 
 
@@ -159,6 +195,7 @@ const bookList = (function(){
       handleNewItemSubmit();
       handleExpandItem();
       handleDeleteItem();
+      handleCondensedItem();
     }
 
 
